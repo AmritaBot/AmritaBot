ccba589ec5ded49db8725db1b9b115dc6ba31ec3
RAX4096 <windowserror@163.com>
2026-08-11 17:06:48 +0800

    Cleanup: Remove dist

--- amrita/plugins/webui/service/static/chunk-g8fjbgvm.js
+++
@@ -1,5 +0,0 @@
-import{db as e,gb as t}from"/static/chunk-j69ax7za.js";function d({className:n,...m}){return e("div",{className:t("animate-pulse rounded-md bg-muted",n),...m})}
-export{d as Wa};
-
-//# debugId=B7E70111F770AE0164756E2164756E21
-//# sourceMappingURL=/static/chunk-g8fjbgvm.js.map
--- amrita/plugins/webui/service/static/chunk-kfm4fzya.js
+++
@@ -1,4 +0,0 @@
-import"/static/chunk-9jgbatjs.js";import"/static/chunk-c6t7b9jx.js";import"/static/chunk-tvbsghq2.js";import"/static/chunk-kqc1kpxw.js";import"/static/chunk-y78zcapq.js";import"/static/chunk-cfzww7d4.js";import"/static/chunk-gdyxfqrm.js";import"/static/chunk-t2ecsbc6.js";import"/static/chunk-sdg6xm0x.js";import"/static/chunk-4jsb0kyx.js";import"/static/chunk-9tq1ah5r.js";import"/static/chunk-8z5tghd8.js";import"/static/chunk-j69ax7za.js";
-
-//# debugId=1E6C91E6EF11C9B964756E2164756E21
-//# sourceMappingURL=/static/chunk-kfm4fzya.js.map
